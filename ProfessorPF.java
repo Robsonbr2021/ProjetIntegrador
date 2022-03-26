3255ca8ce0fb9b1e78a790825f9ff83f3df7c77c
Robsonbr2021 <82483936+Robsonbr2021@users.noreply.github.com>
2022-03-26 19:26:42 -0300

    Update ProfessorPF.java

--- ProfessorPF.java
+++ ProfessorPF.java
@@ -1,2 +1,15 @@
-public class ProfessorPF {
+public class ProfessorPF extends Pessoa{
+    private Disciplina disciplina;
+    private PessoaFisica pessoaFisica;
+
+    public ProfessorPF(Disciplina disciplina, PessoaFisica pessoaFisica) {
+        this.disciplina = disciplina;
+        this.pessoaFisica = pessoaFisica;
+    }
+
+    public ProfessorPF(String nome, String documento, Endereco endereco, Disciplina disciplina, PessoaFisica pessoaFisica) {
+        super(nome, documento, endereco);
+        this.disciplina = disciplina;
+        this.pessoaFisica = pessoaFisica;
+    }
 }
