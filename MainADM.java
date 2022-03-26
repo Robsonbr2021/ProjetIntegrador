7afbe208e6f8151a5413e01c26577739ae2af61b
Robsonbr2021 <82483936+Robsonbr2021@users.noreply.github.com>
2022-03-26 19:22:06 -0300

    Update MainADM.java

--- MainADM.java
+++ MainADM.java
@@ -1,5 +1,41 @@
 public class MainADM {
-    public class void main(String[] args){
+    public static void main(String[] args){
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 10");
+        endereco.setComplemento("casa 2");
+        endereco.setBairro("Casa Verde");
+        System.out.println(endereco.getRua());
+
+        PessoaFisica pessoaF = new PessoaFisica();
+        pessoaF.setNome("Maria Fernanda");
+        pessoaF.setDocumento("RG");
+        pessoaF.setEndereco(endereco);
+
+                System.out.println(pessoaF);
+
+        Disciplina disciplina = new Disciplina();
+        disciplina.setCurso("Química");
+        disciplina.setMateria("Química Orgânica");
+        disciplina.setCurso("Português");
+        disciplina.setMateria("Literatura");
+        System.out.println(disciplina);
+
+        ProfessorPJ PJ = new ProfessorPJ();
+        PJ.setNome("Paulo");
+        PJ.setDocumento("CPF");
+        PJ.setEndereco(endereco);
+        PJ.setValorHora(25.00);
+        PJ.setPagamento(2500.00);
+        System.out.println(PJ);
+
+        Fornecedor fornecedor = new Fornecedor();
+        fornecedor.setNome("Empresa SA");
+        fornecedor.setEndereco(endereco);
+        fornecedor.setPagamento(3000.00);
+        fornecedor.setDocumento("CGC");
+       System.out.println(fornecedor);
+
+
 
     }
 }
