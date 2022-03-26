b53a9a5c04c32d3bee5ab5eba38104c2d8c69949
Robsonbr2021 <82483936+Robsonbr2021@users.noreply.github.com>
2022-03-26 19:27:44 -0300

    Update ProfessorPJ.java

--- ProfessorPJ.java
+++ ProfessorPJ.java
@@ -1,2 +1,27 @@
-public class ProfessorPJ {
+public class ProfessorPJ extends PessoaJuridica{
+
+    private Disciplina disciplina;
+    private Endereco endereco;
+
+    public ProfessorPJ(){
+
+    }
+
+    public ProfessorPJ(Disciplina disciplina, Endereco endereco) {
+        this.disciplina = disciplina;
+        this.endereco = endereco;
+    }
+
+    public ProfessorPJ(double valorHora, double pagamento, Disciplina disciplina, Endereco endereco) {
+        super(valorHora, pagamento);
+        this.disciplina = disciplina;
+        this.endereco = endereco;
+    }
+
+    public ProfessorPJ(String nome, String documento, Endereco endereco, double valorHora, double pagamento, Disciplina disciplina, Endereco endereco1) {
+        super(nome, documento, endereco, valorHora, pagamento);
+        this.disciplina = disciplina;
+        this.endereco = endereco1;
+    }
 }
+
