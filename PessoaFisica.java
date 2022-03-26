6b44aae06fd2be8a48da18b339109f069f08d30f
Robsonbr2021 <82483936+Robsonbr2021@users.noreply.github.com>
2022-03-26 19:24:18 -0300

    Update PessoaFisica.java

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,2 +1,15 @@
-public class PessoaFisica {
+public class PessoaFisica extends Pessoa{
+    private double salario;
+
+    public PessoaFisica() {
+    }
+
+    public PessoaFisica(double salario) {
+        this.salario = salario;
+    }
+
+    public PessoaFisica(String nome, String documento, Endereco endereco, double salario) {
+        super(nome, documento, endereco);
+        this.salario = salario;
+    }
 }
