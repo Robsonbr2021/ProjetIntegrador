f37c9ed44619d224c298f9bbd5b167c65bc5758e
Robsonbr2021 <82483936+Robsonbr2021@users.noreply.github.com>
2022-03-26 19:23:03 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,3 +1,48 @@
-public class Pessoa {
-    
+public abstract class Pessoa {
+    protected String nome;
+    protected String documento;
+    protected Endereco endereco;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String documento, Endereco endereco) {
+        this.nome = nome;
+        this.documento = documento;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", documento='" + documento + '\'' +
+                ", endereco=" + endereco +
+                '}';
+    }
 }
+
