de62f70d959db4d907d8f122742bed0a2bf1b868
Robsonbr2021 <82483936+Robsonbr2021@users.noreply.github.com>
2022-03-26 19:19:44 -0300

    Update Fornecedor.java

--- Fornecedor.java
+++ Fornecedor.java
@@ -1,2 +1,21 @@
-public class Fornecedor {
+public class Fornecedor extends PessoaJuridica{
+    private String produto;
+    private Endereco endereco;
+
+    public Fornecedor(){
+    }
+
+    public Fornecedor(double valorHora, double pagamento, String produto, Endereco endereco) {
+        super(valorHora, pagamento);
+        this.produto = produto;
+        this.endereco = endereco;
+    }
+
+    public Fornecedor(String nome, String documento, Endereco endereco, double valorHora, double pagamento, String produto, Endereco endereco1) {
+        super(nome, documento, endereco, valorHora, pagamento);
+        this.produto = produto;
+        this.endereco = endereco1;
+    }
 }
+
+
